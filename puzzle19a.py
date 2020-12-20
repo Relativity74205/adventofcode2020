c446744f1b0ee8d4b8c8bcfb14ea136d15f1ea4a
Relativity74205 <arkadius@schuchhardt.com>
2020-12-20 17:50:01 +0100

    first

--- puzzle19a.py
+++ puzzle19a.py
@@ -2,6 +2,7 @@ from typing import Dict
 from pathlib import Path
 from itertools import product
 from functools import reduce
+from operator import add
 
 
 with open(Path(__file__).parent / "data" / "puzzle19.txt", "r") as f:
@@ -13,20 +14,11 @@ def parse_rule(rule: str, rules: Dict[int, str]):
         return [rule[1]]
 
     if '|' not in rule:
-        results = []
-        for sub_rule in rule.split(' '):
-            sub_result = parse_rule(rules[int(sub_rule)], rules)
-            results.append(sub_result)
-        result_comb = list(product(*results))
-        result = [reduce(lambda a, b: a + b, r) for r in result_comb]
-        return result
+        results = [parse_rule(rules[int(sub_rule)], rules) for sub_rule in rule.split(' ')]
+        return [reduce(add, r) for r in product(*results)]
     else:
-        sub_rule1 = rule.split(' | ')[0]
-        sub_rule2 = rule.split(' | ')[1]
-        result1 = parse_rule(sub_rule1, rules)
-        result2 = parse_rule(sub_rule2, rules)
-        result = result1 + result2
-        return result
+        sub_results = [parse_rule(sub_rule, rules) for sub_rule in rule.split(' | ')]
+        return reduce(add, sub_results)
 
 
 rule_rows = [row for row in raw_data if ':' in row]
@@ -34,12 +26,10 @@ messages_rows = [row for row in raw_data if ':' not in row and row != '']
 
 rules_dict = {int(rule.split(':')[0]): rule.split(':')[1][1:] for rule in rule_rows}
 
-valid_massages = parse_rule(rules_dict[0], rules_dict)
-valid_massages_set = set(valid_massages)
+valid_massages = set(parse_rule(rules_dict[0], rules_dict))
+message_is_valid = [message in valid_massages for message in messages_rows]
+print(f'{sum(message_is_valid)=}')  # 142
 
-assert ({'aaaabb', 'aaabab', 'abbabb', 'abbbab', 'aabaab', 'aabbbb', 'abaaab', 'ababbb'}
-        == set(parse_rule('4 1 5', {0: '4 1 5', 1: '2 3 | 3 2', 2: '4 4 | 5 5', 3: '4 5 | 5 4', 4: '"a"', 5: '"b"'}))
-        )
-
-message_is_valid = [message in valid_massages_set for message in messages_rows]
-print(f'{sum(message_is_valid)=}')
+# assert ({'aaaabb', 'aaabab', 'abbabb', 'abbbab', 'aabaab', 'aabbbb', 'abaaab', 'ababbb'}
+#         == set(parse_rule('4 1 5', {0: '4 1 5', 1: '2 3 | 3 2', 2: '4 4 | 5 5', 3: '4 5 | 5 4', 4: '"a"', 5: '"b"'}))
+#         )
