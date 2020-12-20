587329e05a5a4f9b0de94854ad97ebe57172d5e1
Relativity74205 <arkadius@schuchhardt.com>
2020-12-20 23:18:01 +0100

    first

--- puzzle19a.py
+++ puzzle19a.py
@@ -9,6 +9,11 @@ with open(Path(__file__).parent / "data" / "puzzle19.txt", "r") as f:
     raw_data = f.read().splitlines()
 
 
+rule_rows = [row for row in raw_data if ':' in row]
+messages_rows = [row for row in raw_data if ':' not in row and row != '']
+rules_dict = {int(rule.split(':')[0]): rule.split(':')[1][1:] for rule in rule_rows}
+
+
 def parse_rule(rule: str, rules: Dict[int, str]):
     if rule in ('"a"', '"b"'):
         return [rule[1]]
@@ -21,15 +26,6 @@ def parse_rule(rule: str, rules: Dict[int, str]):
         return reduce(add, sub_results)
 
 
-rule_rows = [row for row in raw_data if ':' in row]
-messages_rows = [row for row in raw_data if ':' not in row and row != '']
-
-rules_dict = {int(rule.split(':')[0]): rule.split(':')[1][1:] for rule in rule_rows}
-
 valid_massages = set(parse_rule(rules_dict[0], rules_dict))
 message_is_valid = [message in valid_massages for message in messages_rows]
 print(f'{sum(message_is_valid)=}')  # 142
-
-# assert ({'aaaabb', 'aaabab', 'abbabb', 'abbbab', 'aabaab', 'aabbbb', 'abaaab', 'ababbb'}
-#         == set(parse_rule('4 1 5', {0: '4 1 5', 1: '2 3 | 3 2', 2: '4 4 | 5 5', 3: '4 5 | 5 4', 4: '"a"', 5: '"b"'}))
-#         )
