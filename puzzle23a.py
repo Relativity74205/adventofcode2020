58bf28b12767b2e81fa81dffc6f91eb22a000957
Relativity74205 <arkadius@schuchhardt.com>
2020-12-24 23:10:51 +0100

    first

--- puzzle23a.py
+++ puzzle23a.py
@@ -1,56 +1,50 @@
-import typing as ty
+import time
+from typing import List
+from collections import deque
 
 
 input_list = '193467258'
+# moves = 10_000_000
 moves = 100
+max_target_label = 9
 
 
-def get_destination_cup(arr: ty.List, target_label: int) -> int:
+def get_destination_cup(arr: deque, pick_up_values: List, target_label: int) -> int:
     while True:
-        if target_label in arr:
+        target_label -= 1
+        if target_label <= 0:
+            target_label = max_target_label
+        if target_label not in pick_up_values:
             return arr.index(target_label)
-        else:
-            target_label -= 1
-            if target_label <= 0:
-                target_label = 9
 
 
-def move_circle(arr: ty.List, times: int) -> ty.List:
-    if times > 0:
-        for _ in range(times):
-            arr.append(arr[0])
-            arr.remove(arr[0])
-    elif times < 0:
-        for _ in range(times):
-            arr.insert(0, arr[-1])
-            arr.remove(arr[-1])
+def calc_move(arr: deque) -> deque:
+    current_cup_value = arr[0]
+    arr.rotate(-1)
+    pick_up_values = [arr.popleft() for _ in range(3)]
+    destination_cup_index = get_destination_cup(arr, pick_up_values, current_cup_value)
+    arr.rotate(- destination_cup_index - 1)
+    arr.appendleft(pick_up_values[2])
+    arr.appendleft(pick_up_values[1])
+    arr.appendleft(pick_up_values[0])
+    arr.rotate(-(- destination_cup_index - 1))
 
     return arr
 
 
-arr = list(map(int, input_list))
-current_cup_index = 0
-for _ in range(moves):
-    # print(f'{current_cup_index=}')
-    current_cup_value = arr[current_cup_index % 9]
-    pick_up_indices = [ele % 9 for ele in range(current_cup_index + 1, current_cup_index + 4)]
-    pick_up_values = [ele for i, ele in enumerate(arr) if i in pick_up_indices]
-    temp_arr = [ele for i, ele in enumerate(arr) if i not in pick_up_indices]
-    destination_cup_index = get_destination_cup(temp_arr, arr[current_cup_index] - 1)
-    destination_cup_value = temp_arr[destination_cup_index]
-    for i, pick_up_index in enumerate(pick_up_indices):
-        temp_arr.insert(destination_cup_index + i + 1, arr[pick_up_index])
-    new_current_cup_index = temp_arr.index(current_cup_value)
-    # temp_arr = move_circle(temp_arr, new_current_cup_index - current_cup_index)
-    arr = temp_arr.copy()
-    current_cup_index = new_current_cup_index + 1
-    current_cup_index = current_cup_index % 9
-
-
-def get_result(arr: ty.List) -> str:
+def get_result(arr: deque) -> str:
     pos_one = arr.index(1)
     return ''.join([str(arr[(pos_one + 1 + i) % 9]) for i in range(8)])
 
 
-print(f'{get_result(arr)=}')
+arr_start = deque(map(int, input_list))
+arr_output = arr_start.copy()
+
+t1 = time.time()
+for _ in range(moves):
+    arr_output = calc_move(arr_output)
+t2 = time.time()
+
+print(t2-t1)
+print(f'{get_result(arr_output)=}')
 #  25468379
