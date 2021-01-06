731a3cbccde617d7d6b106a21906594ea9f3c519
Relativity74205 <arkadius@schuchhardt.com>
2021-01-06 20:53:16 +0100

    first

---
+++ julia/puzzle03/puzzle03.jl
@@ -0,0 +1,28 @@
+raw_data = readlines("julia/puzzle03/puzzle03.txt")
+
+function solve(data::Vector{String}, strategy::Tuple{Int, Int})::Int
+    x, y = 1, 1
+    height = length(data)
+    width = length(data[1])
+
+    amount_trees::Int = 0
+    while 1 > 0
+        if y > height
+            return amount_trees
+        end
+        if x > width
+            x -= width
+        end
+
+        amount_trees += data[y][x] == '#'
+        x += strategy[1]
+        y += strategy[2]
+    end
+
+end
+
+result_a = solve(raw_data, (3, 1))
+results_b = (solve(raw_data, strategy) for strategy in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)))
+
+println("Result for A is $result_a")
+println("Result for B is $(prod(results_b))")
